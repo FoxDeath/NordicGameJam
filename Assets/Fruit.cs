97367dd8c372f5b263f85b09b91a31072e2df803
Radu Matusa <matusaradu@gmail.com>
2020-04-17 21:32:25 +0200

    Fruit be despawning

--- Assets/Fruit.cs
+++ Assets/Fruit.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    private bool onLine = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Destroy());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(5f);
+
+        if(!onLine)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            yield return StartCoroutine(Destroy());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Line"))
+        {
+            onLine = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Line"))
+        {
+            onLine = false;
+        }
     }
 }
