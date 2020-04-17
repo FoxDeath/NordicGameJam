f95ace89af7c877a976cb497715a9e12b37ae912
Andrei Mataoanu <amatao1999@gmail.com>
2020-04-17 21:43:23 +0200

    Enemy follows player, avoids obstacles, doesn't move on axis yet.

---
+++ Assets/OfficePacman/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform player;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        FaceTarget();
+        agent.destination = player.transform.position;
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            //Player dies or game restarts.
+        }
+    }
+}
