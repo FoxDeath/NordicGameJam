050579adc209d399504ab19e80b3303a2a131f80
Tomi Faludi <tomifaludi@gmail.com>
2020-04-17 23:41:15 +0200

    terrain generation basics

---
+++ Assets/Scripts/CrossRoad/TerrainGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    private static TerrainGenerator instance;
+    [SerializeField] List<GameObject> terrains = new List<GameObject>();
+
+    private Vector3 currentPosition = new Vector3(0f, -1f, 0f);
+
+    [SerializeField] int levelLength;
+
+    public static TerrainGenerator GetInstance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        for (int i = 0; i <= levelLength; i++)
+        {
+            SpawnTerrain();
+        }
+    }
+
+    public void SpawnTerrain()
+    {
+        GameObject currentTerrain = Instantiate(terrains[Random.Range(0, terrains.Count)], currentPosition, Quaternion.identity, transform);
+        
+        if (currentTerrain.CompareTag("Road"))
+        {
+            currentPosition.z += 2f;
+        }
+        else
+        {
+            currentPosition.z++;
+        }
+    }
+}
