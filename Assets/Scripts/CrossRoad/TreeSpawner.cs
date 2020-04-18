4193b3a7629000e1eecd37dcb672619d3cb6e4e7
Tomi Faludi <tomifaludi@gmail.com>
2020-04-18 22:02:39 +0200

    restriction and trees

---
+++ Assets/Scripts/CrossRoad/TreeSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject tree;
+
+    [SerializeField] int aprxAmount = 15;
+
+    void Start()
+    {
+        aprxAmount += Random.Range(-3, 3);
+
+        for (int i = 0; i < aprxAmount; i++)
+        {
+            Instantiate(tree, new Vector3(transform.position.x + Random.Range(-22, 22), transform.position.y + 1.2f, transform.position.z), Quaternion.identity, transform);
+        }
+    }
+}
